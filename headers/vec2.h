3ec29dd1d4575122e99b12c174212c9ada81f1e5
Nicolas Faucheux <nicolas.faucheux@etu.unistra.fr>
2020-03-17 04:07:22 +0100

    operateur de comparaison

--- headers/vec2.h
+++ headers/vec2.h
@@ -17,7 +17,15 @@ class Vec2
             return Vec2(_x + v._x, _y + v._y);
         }
 
-        friend std::ostream& operator<<(std::ostream& os, Vec2 const& v) {
+        friend bool operator==(const Vec2& v1, const Vec2& v2) {
+            return (v1._x == v2._x && v1._y == v2._y);
+        }
+
+        friend bool operator!=(const Vec2& v1, const Vec2& v2) {
+            return !(v1 == v2);
+        }
+
+        friend std::ostream& operator<<(std::ostream& os, const Vec2& v) {
             os << "[" << v._x << ", " << v._y << "]";
             return os;
         }
--- main.cpp
+++ main.cpp
@@ -45,12 +45,16 @@ int main(int argc, char** argv)
 	g.init(renderer);
 
 	Vec2<int> v1(5,6);
-	Vec2<int> v2(10,10);
+	Vec2<int> v2(5,10);
 	Vec2<int> v3 = v1 + v2;
 
 	std::cout << "v1 = " << v1 << std::endl;
 	std::cout << "v2 = " << v2 << std::endl;
 	std::cout << "v3 = " << v3 << std::endl;
+
+	std::cout << "v1 == v2 = " << (v1==v2) << std::endl;
+
+
 	
 	// std::cout << "[" << v._x << ", " << v._y << "]" << std::endl;
 
